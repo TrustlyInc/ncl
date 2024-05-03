5f40ceae1cb5eb144ef7f2c43ad800996740a0b1
henrique.jesus <henrique.jesus@trustly.com>
2024-05-03 14:51:08 -0300

    Fixing tabs

--- ncl-core/src/main/java/com/paywithmybank/ncl/NCL.java
+++ ncl-core/src/main/java/com/paywithmybank/ncl/NCL.java
@@ -10,57 +10,57 @@ import com.paywithmybank.ncl.model.Source;
 
 public class NCL {
 
-	private final Map<String,Module> modules = new HashMap<>();
+    private final Map<String,Module> modules = new HashMap<>();
 
     private final ModuleLoader loader;
 
-	public NCL() {
-		loader = new ClassModuleLoader();
-	}
+    public NCL() {
+        loader = new ClassModuleLoader();
+    }
 
-	public NCL(String path) {
-		loader = new ClassModuleLoader(ClassModuleLoader.class, path);
-	}
-	
-	public synchronized Module loadModule(Source source) {
-		Module module = new Module(source,this);
-		
-		modules.put(module.getName(),module);
-		
-		return module;
-	}
-	
-	public synchronized Module loadModule(String source) {
-		Module module = new Module(source,this);
-		
-		modules.put(module.getName(),module);
-		
-		return module;
-	}
-	
-	public Module getModule(String name){
-		Module module = modules.get(name);
-		
-		if(module == null){
-			module = loadModule(loader.openModule(name));
-		}
-		
-		return module;
-	}
+    public NCL(String path) {
+        loader = new ClassModuleLoader(ClassModuleLoader.class, path);
+    }
+    
+    public synchronized Module loadModule(Source source) {
+        Module module = new Module(source,this);
+        
+        modules.put(module.getName(),module);
+        
+        return module;
+    }
+    
+    public synchronized Module loadModule(String source) {
+        Module module = new Module(source,this);
+        
+        modules.put(module.getName(),module);
+        
+        return module;
+    }
+    
+    public Module getModule(String name){
+        Module module = modules.get(name);
+        
+        if(module == null){
+            module = loadModule(loader.openModule(name));
+        }
+        
+        return module;
+    }
 
-	public static void main(String[] args) {
-		
-		NCL ncl = new NCL();
+    public static void main(String[] args) {
+        
+        NCL ncl = new NCL();
 
-		long start = System.currentTimeMillis();				
-		//System.out.println(new Parser(code).parse().toString());
-		
-		Module module;
-		module = ncl.getModule("fb");
-		System.out.println("Load time: " + (System.currentTimeMillis() - start));
-		start = System.currentTimeMillis();	
-		Node node = module.getNode("page#select_bank");
-		System.out.println("Apply rules time: " + (System.currentTimeMillis() - start) + "\n");
-		System.out.println(node.toString());
-	}
+        long start = System.currentTimeMillis();                
+        //System.out.println(new Parser(code).parse().toString());
+        
+        Module module;
+        module = ncl.getModule("fb");
+        System.out.println("Load time: " + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();    
+        Node node = module.getNode("page#select_bank");
+        System.out.println("Apply rules time: " + (System.currentTimeMillis() - start) + "\n");
+        System.out.println(node.toString());
+    }
 }
--- ncl-core/src/main/java/com/paywithmybank/ncl/loader/ClassModuleLoader.java
+++ ncl-core/src/main/java/com/paywithmybank/ncl/loader/ClassModuleLoader.java
@@ -7,39 +7,39 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class ClassModuleLoader implements ModuleLoader {
-	Class<?> loader;
-	String path;
-	
-	public ClassModuleLoader() {
-		this.loader = this.getClass();
-		this.path = null;
-	}
-	
-	public ClassModuleLoader(Class<?> loader, String path) {
-		this.loader = loader;
-		this.path = formatPath(path);
-	}
+    Class<?> loader;
+    String path;
 
-	@Override
-	public Source openModule(String filename) {
-        try {
-            if (path == null || path.trim().isEmpty()) {
-                String fileName = "/" + filename + ".ncl";
-                return new Source (fileName , loader.getResourceAsStream(fileName));
-            } else {
-                String fileName = path + filename + ".ncl";
-                return new Source(fileName, Files.newInputStream(Paths.get(fileName)));
-            }
+    public ClassModuleLoader() {
+            this.loader = this.getClass();
+            this.path = null;
+    }
+
+    public ClassModuleLoader(Class<?> loader, String path) {
+            this.loader = loader;
+            this.path = formatPath(path);
+    }
+
+    @Override
+    public Source openModule(String filename) {
+    try {
+        if (path == null || path.trim().isEmpty()) {
+            String fileName = "/" + filename + ".ncl";
+            return new Source (fileName , loader.getResourceAsStream(fileName));
+        } else {
+            String fileName = path + filename + ".ncl";
+            return new Source(fileName, Files.newInputStream(Paths.get(fileName)));
+        }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-	}
+    }
 
     /*
      * Guarantees that the path ends with \ or /,
      * so that we can append the file name after.
      */
-	private static String formatPath(String path) {
+        private static String formatPath(String path) {
         char lastChar = path.charAt(path.length() - 1);
         if (lastChar == '\\' || lastChar == '/') {
             return path;
